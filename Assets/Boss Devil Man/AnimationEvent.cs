3d6b47d385a63b8d48d38bf6866f5294762ac68d
SonSeongBin <thstjdqls2625@gmail.com>
2023-02-15 22:24:33 +0900

    보스 패턴

--- Assets/Boss Devil Man/AnimationEvent.cs
+++ Assets/Boss Devil Man/AnimationEvent.cs
@@ -2,33 +2,41 @@
 using System;
 using System.Collections;
 
-//Just for demonstration, you can replace it with your own code logic.
-//public class AnimationEvent : MonoBehaviour {
+public class AnimationEvent : MonoBehaviour
+{
 
-//	public GameObject enemy;
+    public GameObject enemy;
 
-//	private int atkTimes = 0;
+    private int atkTimes = 0;
 
-//	public void AttackStart () {
-//		Debug.Log ("Attack Start");
+    public void AttackStart()
+    {
+        Debug.Log("Attack Start");
 
-//		//Just for demonstration, you can replace it with your own code logic.
-//		atkTimes++;
-//		if (enemy && atkTimes <= 3) {
-//			Animator enemyAnimator = enemy.GetComponent<Animator> ();
-//			if (atkTimes == 1) {
-//				enemyAnimator.SetTrigger ("hit_1");
-//			} else if (atkTimes == 2) {
-//				enemyAnimator.SetTrigger ("hit_2");
-//			} else if (atkTimes == 3) {
-//				enemyAnimator.SetTrigger ("hit_2");
-//				enemyAnimator.SetTrigger ("death");
-//			} 
-//		}
-//	}
+        //Just for demonstration, you can replace it with your own code logic.
+        atkTimes++;
+        if (enemy && atkTimes <= 3)
+        {
+            Animator enemyAnimator = enemy.GetComponent<Animator>();
+            if (atkTimes == 1)
+            {
+                enemyAnimator.SetTrigger("hit_1");
+            }
+            else if (atkTimes == 2)
+            {
+                enemyAnimator.SetTrigger("hit_2");
+            }
+            else if (atkTimes == 3)
+            {
+                enemyAnimator.SetTrigger("hit_2");
+                enemyAnimator.SetTrigger("death");
+            }
+        }
+    }
 
-//	public void AttackStartEffectObject () {
-//		Debug.Log ("Fire Effect Object");
-//	}
+    public void AttackStartEffectObject()
+    {
+        Debug.Log("Fire Effect Object");
+    }
 
-//}
+}
