68a5af7d97a1dbf62d538fbc7262244b863c1a8c
Ggm-gijoo <gijoo1012@gmail.com>
2023-03-01 19:07:46 +0900

    변수 둬서 원하는 맵으로 만들 수 있도록 통제

--- Assets/01.Scripts/Test/LoadMapTest.cs
+++ Assets/01.Scripts/Test/LoadMapTest.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Stage
+{
+    Power,
+    Ghost,
+    Lava,
+    Electric,
+    Wolf,
+    Lizard
+}
+
 public class LoadMapTest : MonoBehaviour
 {
+    [Space]
+    [SerializeField] private Stage stage;
     void Start()
     {
-        Managers.Resource.Instantiate($"03.Prefabs/Maps/Lava/Lava_MonsterZone{Random.Range(1,5)}",transform);
+        Managers.Resource.Instantiate($"03.Prefabs/Maps/{stage}/{stage}_MonsterZone{Random.Range(1,5)}",transform);
     }
 
 }
