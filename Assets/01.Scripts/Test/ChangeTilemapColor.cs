126ff052e2a6f18d5057457b49fd416471fb7c26
SonSeongBin <thstjdqls2625@gmail.com>
2023-03-08 00:35:19 +0900

    보스 궁극기 밑 보스 스킬

--- Assets/01.Scripts/Test/ChangeTilemapColor.cs
+++ Assets/01.Scripts/Test/ChangeTilemapColor.cs
@@ -8,35 +8,30 @@ public class ChangeTilemapColor : MonoBehaviour
     public Transform Boss;
     public Tilemap tileMap;
     private Grid grid;
-    private Vector3Int cellPos;
+    private Vector3Int RC; //RangeCell
+    private Vector3Int RCF; //RangeCellFill
 
     private Vector3 pos;
+
+    WaitForSeconds RCFTime = new WaitForSeconds(0.3f);
 
     private void Start()
     {
         grid = GetComponent<Grid>();
-        ChangeTileColor(Boss);
     }
 
-    public void CellChange()
+    public void RCChange()
     {
-        tileMap.SetTileFlags(cellPos, TileFlags.None);
-        tileMap.SetColor(cellPos, Color.red);
+        tileMap.SetTileFlags(RC, TileFlags.None);
+        tileMap.SetColor(RC, new Color(255,0,0,1));
+    }
+
+    public void RCFChange()
+    {
+        tileMap.SetTileFlags(RCF, TileFlags.None);
+        tileMap.SetColor(RCF, new Color(255, 0, 0, 1));
     }
         
-    public void ChangeTileColor(Transform tileTransform)
-    {
-        pos = tileTransform.position;
-        cellPos = grid.WorldToCell(pos);
 
-        cellPos.x -= 1;
-        cellPos.y -= 1;
-
-        cellPos.x -= 5; cellPos.y -= 2; CellChange();
-        cellPos.y += 1;CellChange();
-        cellPos.y += 1; CellChange();
-        cellPos.y += 1; CellChange();
-
-
-    }
 }
+
