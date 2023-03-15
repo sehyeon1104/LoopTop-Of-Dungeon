03cbefc4e5a0010a62254062357b0d080113f22e
Ggm-gijoo <gijoo1012@gmail.com>
2023-03-16 00:33:30 +0900

    빔 이펙트 제작

---
+++ Assets/01.Scripts/Test/BeamPatternTest.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamPatternTest : MonoBehaviour
+{
+    [SerializeField]GameObject startFX;
+    [SerializeField] LineRenderer beam;
+
+    float lineLength = 0.0f;
+    float lineWidth = 0.5f;
+
+    private void Awake()
+    {
+   
+    }
+    void Start()
+    {
+        StartCoroutine(testPlay());
+    }
+
+    private IEnumerator testPlay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2f);
+
+            lineWidth = 0.5f;
+
+            beam.startWidth = lineWidth;
+            beam.endWidth = lineWidth;
+
+            beam.SetPosition(1, Vector3.zero);
+
+            CinemachineCameraShaking.Instance.CameraShake(5, 0.4f);
+
+            startFX.GetComponentInChildren<ParticleSystem>().Play();
+
+            while (beam.GetPosition(1).x <= 50)
+            {
+                lineLength += 0.5f;
+                beam.SetPosition(1, new Vector3(lineLength, 0, 0));
+
+                yield return null;
+            }
+
+            lineLength = 0;
+
+            while(lineWidth >= 0.0f)
+            {
+                lineWidth -= Time.deltaTime;
+
+                beam.startWidth = lineWidth;
+                beam.endWidth = lineWidth;
+
+                yield return null;
+            }
+            beam.startWidth = 0;
+            beam.endWidth = 0;
+
+        }
+
+    }
+}
