d307350596fa5a3850616640e7e1b07cb9432c10
Ggm-gijoo <gijoo1012@gmail.com>
2023-03-20 18:36:17 +0900

    빔이펙트 라이트 추가

--- Assets/01.Scripts/Test/BeamPatternTest.cs
+++ Assets/01.Scripts/Test/BeamPatternTest.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BeamPatternTest : MonoBehaviour
 {
-    [SerializeField]GameObject startFX;
+    [SerializeField] GameObject startFX;
     [SerializeField] LineRenderer beam;
+    [SerializeField] Light2D beamLight;
 
     float lineLength = 0.0f;
     float lineWidth = 0.5f;
 
     [SerializeField] float length;
     [SerializeField] float width;
+    [SerializeField] float intensity = 2f;
 
     private void Awake()
     {
@@ -35,10 +38,14 @@ public class BeamPatternTest : MonoBehaviour
 
             beam.SetPosition(1, Vector3.zero);
 
+            while(beamLight.intensity <= intensity)
+            {
+                beamLight.intensity += 0.01f;
+                yield return null;
+            }
             CinemachineCameraShaking.Instance.CameraShake(5, 0.4f);
 
             startFX.GetComponentInChildren<ParticleSystem>().Play();
-
             while (beam.GetPosition(1).x <= length)
             {
                 lineLength += 0.5f;
@@ -53,11 +60,13 @@ public class BeamPatternTest : MonoBehaviour
             {
                 lineWidth -= Time.deltaTime;
 
+                beamLight.intensity -= 0.01f;
                 beam.startWidth = lineWidth;
                 beam.endWidth = lineWidth;
 
                 yield return null;
             }
+            beamLight.intensity = 0;
             beam.startWidth = 0;
             beam.endWidth = 0;
 
