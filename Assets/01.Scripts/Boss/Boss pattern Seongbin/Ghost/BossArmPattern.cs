4e0b34dcb6bc19e78aeb563d0088cc86c9855e28
SonSeongBin <thstjdqls2625@gmail.com>
2023-03-21 14:55:34 +0900

    고스트 애니메이션 수정

--- Assets/01.Scripts/Boss/Boss pattern Seongbin/Ghost/BossArmPattern.cs
+++ Assets/01.Scripts/Boss/Boss pattern Seongbin/Ghost/BossArmPattern.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class BossArmPattern : MonoBehaviour
 {
-    
+    public Vector2 size1;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, size1);
+    }
 }
