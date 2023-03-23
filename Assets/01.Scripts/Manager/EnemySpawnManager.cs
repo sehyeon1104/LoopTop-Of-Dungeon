5b842924750edcf3c676117499f3b8605a317bf6
Minkyu Kwak <lanilani7661@gmail.com>
2023-03-23 09:21:31 +0900

    적 프리팹 다른 방법으로 불러오기 (주석)

--- Assets/01.Scripts/Manager/EnemySpawnManager.cs
+++ Assets/01.Scripts/Manager/EnemySpawnManager.cs
@@ -50,6 +50,9 @@ public class EnemySpawnManager : MonoSingleton<EnemySpawnManager>
     }
 
     #region Addressable
+
+    // SO에 몹 배열을 선언, 해당 SO에 잡몹 넣고 어드레서블 적용. 그리고 사용할 때 SO에 있는 몹 불러오기
+
     public void GetLocations()
     {
         // 빌드타겟의 경로를 가져온다.
