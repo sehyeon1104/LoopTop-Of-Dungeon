f52d9c3cbbb5d01b847f9b8d193f0e25179ab074
Minkyu Kwak <lanilani7661@gmail.com>
2023-03-31 00:25:08 +0900

    빔패턴 1레벨 코드 완성 ( 에셋 변경 필요 )

---
+++ Assets/01.Scripts/Test/PlayerBeamColCheck.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBeamColCheck : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") || collision.CompareTag("Boss"))
+        {
+            collision.GetComponent<IHittable>().OnDamage(2, GameManager.Instance.Player.gameObject, GameManager.Instance.Player.playerBase.CritChance);
+        }
+    }
+}
