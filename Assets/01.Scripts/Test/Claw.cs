e39ce0569659798a9bffdb9cb54cb3b2f133bfdc
Ggm-gijoo <gijoo1012@gmail.com>
2023-03-30 11:34:20 +0900

    메인카메라 프리팹화

--- Assets/01.Scripts/Test/Claw.cs
+++ Assets/01.Scripts/Test/Claw.cs
@@ -7,7 +7,6 @@ public class Claw : MonoBehaviour
     private void OnEnable()
     {
         Collider2D[] cols = Physics2D.OverlapBoxAll(transform.position, new Vector2(transform.lossyScale.y * 2, transform.lossyScale.x * 2), transform.rotation.z);
-        Debug.Log(transform.lossyScale);
         foreach (var col in cols)
         {
             if (col.CompareTag("Player"))
@@ -15,12 +14,5 @@ public class Claw : MonoBehaviour
                 GameManager.Instance.Player.OnDamage(2, gameObject, 0);
             }
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.color = Color.red;
-        Gizmos.DrawCube(transform.position, new Vector2(transform.lossyScale.y * 2, transform.lossyScale.x * 2));
     }
 }
