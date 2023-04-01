16e5525d113240388e0a3fb7d310b19e235e5265
Ggm-gijoo <gijoo1012@gmail.com>
2023-04-01 22:41:33 +0900

    흠,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,

--- Assets/01.Scripts/Scene/Base.cs
+++ Assets/01.Scripts/Scene/Base.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class Base : MonoSingleton<Base>
 {
-    public Define.Scene SceneType { get; protected set; } = Define.Scene.Unknown;
+    public Define.Scene SceneType = Define.Scene.Unknown;
     public Define.MapTypeFlag MapType = Define.MapTypeFlag.Default;
 
     private void Awake()
