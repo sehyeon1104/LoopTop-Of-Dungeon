9047c72394f81e212fda3b163dbce47cc550ef77
Ggm-gijoo <gijoo1012@gmail.com>
2023-04-27 18:42:15 +0900

    고스트 힐라패턴 소환 오류 해결

--- Assets/01.Scripts/Boss/Boss_AI/GhostPattern.cs
+++ Assets/01.Scripts/Boss/Boss_AI/GhostPattern.cs
@@ -165,7 +165,7 @@ public class G_Patterns : BossPattern
         Managers.Pool.PoolManaging("10.Effects/ghost/Absorb", transform.position + Vector3.down * 2f, Quaternion.identity);
         for (int i = 0; i < count; i++)
         {
-            Poolable clone = Managers.Pool.PoolManaging("03.Prefabs/Enemy/Ghost/G_Mob_01", new Vector2(Random.Range(-2.5f, 29.5f), Random.Range(-3, 17.5f)), Quaternion.identity);
+            Poolable clone = Managers.Pool.PoolManaging("Assets/03.Prefabs/Enemy/Ghost/Normal/G_Mob_01.prefab", new Vector2(Random.Range(-2.5f, 29.5f), Random.Range(-3, 17.5f)), Quaternion.identity);
             mobList.Add(clone);
         }
 
