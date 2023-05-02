5167bacb14273e8c370fb126cbfbbb122c7a58c4
Ggm-gijoo <gijoo1012@gmail.com>
2023-05-02 18:56:06 +0900

    돌진 2페이즈

--- Assets/01.Scripts/Test/DashParts.cs
+++ Assets/01.Scripts/Test/DashParts.cs
@@ -1,23 +1,39 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class DashParts : MonoBehaviour
 {
+    Vector3 startPos;
+
     GameObject warning;
     GameObject bar;
 
     SpriteRenderer barRenderer;
     Color mainColor;
 
+    int xDir = 0;
+
     private void Awake()
     {
+        startPos = transform.position;
+
         warning = transform.Find("Warning").gameObject;
         bar = warning.transform.Find("Bar").gameObject;
 
         barRenderer = bar.GetComponentInChildren<SpriteRenderer>();
         mainColor = barRenderer.color;
+
+        string name = gameObject.name;
+        string result = Regex.Replace(name, @"[^0-9]", "");
+
+        for(int i = 0; i < result.Length; i++)
+        {
+            xDir = (result[i] - '0') % 2 == 1 ? 1 : -1;
+
+        }
     }
 
     private void OnEnable()
@@ -28,6 +44,8 @@ public class DashParts : MonoBehaviour
     private IEnumerator OnDash()
     {
         yield return null;
+        transform.position = startPos;
+
         warning.SetActive(true);
         bar.transform.localScale = new Vector3(1, 0);
 
@@ -41,6 +59,7 @@ public class DashParts : MonoBehaviour
         barRenderer.color = mainColor;
         warning.SetActive(false);
 
-
+        transform.DOMoveX(transform.position.x + xDir * 50f, 0.5f);
+        yield return new WaitForSeconds(1f);
     }
 }
