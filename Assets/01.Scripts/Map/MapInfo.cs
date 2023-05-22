c419d845102bef9f8176baab63dab4b2dc72cc49
Minkyu Kwak <lanilani7661@gmail.com>
2023-05-22 20:25:17 +0900

    맵 정보 배열 선언

--- Assets/01.Scripts/Map/MapInfo.cs
+++ Assets/01.Scripts/Map/MapInfo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class MapInfo
 {
-    public static int[,] WallGrid { get; private set; } =new int[7, 7]
+    public static int[,] WallGrid1 { get; private set; } =new int[7, 7]
     {
         {1, 0, 0, 0, 0, 0, 0},
         {2, 0, 0, 0, 0, 0, 0},
@@ -13,6 +13,40 @@ public static class MapInfo
         {1, 2, 1, 2, 1, 0, 1},
         {2, 0, 2, 0, 2, 0, 2},
         {1, 2, 1, 0, 1, 2, 1}
+    };
+
+    public static int[,] WallGrid2 { get; private set; } = new int[7, 7]
+    {
+        { 0, 0, 0, 0, 1, 0, 0 },
+        { 0, 0, 0, 0, 2, 0, 0 },
+        { 0, 0, 1, 2, 1, 0, 1 },
+        { 0, 0, 2, 0, 2, 0, 2 },
+        { 1, 2, 1, 2, 1, 0, 1 },
+        { 2, 0, 2, 0, 2, 0, 2 },
+        { 1, 2, 1, 0, 1, 2, 1 }
+
+    };
+
+    public static int[,] WallGrid3 { get; private set; } = new int[7, 7]
+    {
+        {1, 0, 1, 2, 1, 0, 0}  ,
+        {2, 0, 2, 0, 2, 0, 0}  ,
+        {1, 0, 1, 2, 1, 0, 0}  ,
+        {2, 0, 2, 0, 0, 0, 0}  ,
+        {1, 2, 1, 0, 0, 0, 0}  ,
+        {2, 0, 2, 0, 0, 0, 0 } ,
+        {1, 2, 1, 2, 1, 2, 1 }
+    };
+
+    public static int[,] WallGrid4 { get; private set; } = new int[7, 7]
+    {
+        {1, 0, 1, 2, 1, 0, 0},
+        {2, 0, 2, 0, 2, 0, 0},
+        {1, 2, 1, 2, 1, 0, 0},
+        {2, 0, 2, 0, 2, 0, 0},
+        {1, 2, 1, 0, 1, 0, 0},
+        {1, 2, 1, 2, 1, 0, 0},
+        {2, 0, 2, 0, 0, 0, 0}
     };
 
 
