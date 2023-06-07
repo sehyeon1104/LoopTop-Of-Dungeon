c0b8a12c6ec044c99af18e0d34fcaae30b4c31bb
NEWSEHYON1104\sehye <sehyeon1104@naver.com>
2023-06-08 00:53:57 +0900

    새로운 이펙트 추가

--- Assets/01.Scripts/Player/PlayerSkill/Power/PowerSkill.cs
+++ Assets/01.Scripts/Player/PlayerSkill/Power/PowerSkill.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class PowerSkill : PlayerSkillBase
 {
 
-    float choppingDmg = 10;
+    float choppingDmg = 20;
+    float rushDmg = 13;
+    float rushVelocity = 2;
+    float rushDuration = 2f;
+    WaitForFixedUpdate rushWait = new WaitForFixedUpdate();
     ParticleSystem attackPar;
     private void Awake()
     {
@@ -22,7 +26,6 @@ public class PowerSkill : PlayerSkillBase
     }
     protected override void Attack()
     {
-        print("ss");
         if (!playerAnim.GetCurrentAnimatorStateInfo(0).IsName("Idle") || player.playerBase.IsPDead)
             return;
         Managers.Sound.Play("Assets/05.Sounds/SoundEffects/Player/Power/Attack.wav");
@@ -42,7 +45,7 @@ public class PowerSkill : PlayerSkillBase
         {
             if (enemys[i].gameObject.CompareTag("Enemy") || enemys[i].gameObject.CompareTag("Boss"))
             {
-                CinemachineCameraShaking.Instance.CameraShake(5,0.3f);
+                CinemachineCameraShaking.Instance.CameraShake(5, 0.3f);
                 enemys[i].GetComponent<IHittable>().OnDamage(GameManager.Instance.Player.playerBase.Damage, GameManager.Instance.Player.playerBase.CritChance);
             }
         }
@@ -57,27 +60,27 @@ public class PowerSkill : PlayerSkillBase
 
     protected override void SecondSkill(int level)
     {
-        
+        StartCoroutine(Rush());
     }
 
     protected override void ThirdSkill(int level)
     {
-       
+
     }
 
     protected override void ForuthSkill(int level)
     {
-       
+
     }
 
     protected override void FifthSkill(int level)
     {
-        
+
     }
 
     protected override void UltimateSkill()
     {
-        
+
     }
     protected override IEnumerator Dash()
     {
@@ -112,12 +115,47 @@ public class PowerSkill : PlayerSkillBase
     IEnumerator BottomingOut()
     {
         CinemachineCameraShaking.Instance.CameraShake(10, 0.2f);
-        Poolable choppingObj = Managers.Pool.PoolManaging("Assets/10.Effects/player/Power/BottomingOutEffect.prefab", transform.position,Quaternion.identity);
-        Collider2D[] enemys = Physics2D.OverlapCircleAll(choppingObj.transform.position, 2,1<<enemyLayer);
-        for(int i =0;  i<enemys.Length; i++)
+        Poolable choppingObj = Managers.Pool.PoolManaging("Assets/10.Effects/player/Power/BottomingOutEffect.prefab", transform.position, Quaternion.identity);
+        Collider2D[] enemys = Physics2D.OverlapCircleAll(choppingObj.transform.position, 2, 1 << enemyLayer);
+        for (int i = 0; i < enemys.Length; i++)
         {
             enemys[i].GetComponent<IHittable>().OnDamage(choppingDmg, 0);
         }
+        yield return null;
+    }
+    IEnumerator Rush()
+    {
+        float timer = 0;
+        float tickTimer = 0;
+        float dmgTickTime = rushDuration / 5;
+        playerMovement.IsMove = false;
+        player.IsInvincibility = true;
+        playerRigid.velocity = playerMovement.Direction * rushVelocity;
+        do
+        {
+            timer += Time.fixedDeltaTime;
+            tickTimer += Time.fixedDeltaTime;
+            if (tickTimer > dmgTickTime)
+            {
+                CinemachineCameraShaking.Instance.CameraShake(5, 0.2f);
+                Poolable choppingObj = Managers.Pool.PoolManaging("Assets/10.Effects/player/Power/RushEffect.prefab", transform.position, Quaternion.identity);
+                Collider2D[] enemys = Physics2D.OverlapCircleAll(choppingObj.transform.position, 1.5f, 1 << enemyLayer);
+                for (int i = 0; i < enemys.Length; i++)
+                {
+                    enemys[i].GetComponent<IHittable>().OnDamage(rushDmg, 0);
+                }
+                tickTimer = 0;
+            }
+            yield return rushWait;
+        }
+        while (timer < rushDuration);
+        playerMovement.IsMove = true;
+        player.IsInvincibility = false;
+        playerRigid.velocity = Vector3.zero;
+        yield return null;
+    }
+    IEnumerator Jump()
+    {
         yield return null;
     }
     #endregion
