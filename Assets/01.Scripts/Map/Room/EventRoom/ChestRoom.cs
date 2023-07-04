a2d4d80535c3d8b370cf979946b89acb4a7bea4b
Minkyu Kwak <lanilani7661@gmail.com>
2023-07-04 19:39:59 +0900

    상자방 아이콘 추가

--- Assets/01.Scripts/Map/Room/EventRoom/ChestRoom.cs
+++ Assets/01.Scripts/Map/Room/EventRoom/ChestRoom.cs
@@ -6,10 +6,22 @@ public class ChestRoom : RoomBase
 {
     private int spawnChestCount = 3;
     private GameObject chestObj = null;
+    private GameObject minimapIcon = null;
 
     private List<Chest> chestList = new List<Chest>();
 
     private WaitForEndOfFrame waitForEndOfFrame = new WaitForEndOfFrame();
+
+    protected override void Awake()
+    {
+        minimapIcon = Managers.Resource.Instantiate("Assets/03.Prefabs/MinimapIcon/ChestRoomIcon.prefab", transform);
+        minimapIcon.transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y + 1.5f);
+        minimapIcon.SetActive(false);
+
+        minimapIconSpriteRenderer = transform.parent.Find("MinimapIcon").GetComponent<SpriteRenderer>();
+        minimapIconSpriteRenderer.gameObject.SetActive(false);
+        curLocatedMapIcon = transform.parent.Find("CurLocatedIcon").gameObject;
+    }
 
     private void Start()
     {
@@ -20,6 +32,12 @@ public class ChestRoom : RoomBase
     protected override void IsClear()
     {
         isClear = true;
+    }
+
+    protected override void ShowIcon()
+    {
+        Debug.Log("ShowIcon");
+        minimapIcon.SetActive(true);
     }
 
     protected override void SetRoomTypeFlag()
