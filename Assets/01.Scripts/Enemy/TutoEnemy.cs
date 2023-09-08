0acab80e6ee7417f5b59123d7c94b1dbd97356f9
Minkyu Kwak <lanilani7661@gmail.com>
2023-09-08 15:25:50 +0900

    튜토리얼 적 스크립트 생성

--- Assets/01.Scripts/Enemy/TutoEnemy.cs
+++ Assets/01.Scripts/Enemy/TutoEnemy.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutoEnemy : MonoBehaviour
+public class TutoEnemy : EnemyDefault
 {
-    // Start is called before the first frame update
-    void Start()
+    WaitForSeconds attackWait = new WaitForSeconds(2f);
+    WaitForSeconds waitTime = new WaitForSeconds(0.5f);
+
+    private bool isDead = false;
+
+    public override IEnumerator MoveToPlayer()
     {
-        
+        return base.MoveToPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override IEnumerator AttackToPlayer()
     {
-        
+        yield return base.AttackToPlayer();
+
+        yield return waitTime;
+
+        Collider2D col = Physics2D.OverlapCircle(transform.position, 1, 1 << 8);
+        if (col != null)
+            GameManager.Instance.Player.OnDamage(damage, 0);
+
+        yield return attackWait;
+
+        actCoroutine = null;
+
+    }
+
+    public override void OnDamage(float damage, float critChance, Poolable hitEffect = null)
+    {
+        base.OnDamage(damage, critChance);
+    }
+
+    public override void EnemyDead()
+    {
+        if (!isDead)
+        {
+            isDead = true;
+            Managers.Sound.Play("Assets/05.Sounds/SoundEffects/Mob/Mob_DeSpawn.wav");
+
+            CinemachineCameraShaking.Instance.CameraShake(8, 0.2f);
+
+            TutorialManager.Instance.tutoEnemyRoom.EnemyDead(gameObject);
+
+            Managers.Pool.Push(poolable);
+        }
     }
 }
