b0793c977ead5e6fa785ff88dc0c6b59aad91961
Minkyu Kwak <lanilani7661@gmail.com>
2023-09-11 11:00:09 +0900

    헌혈방 구현

--- Assets/01.Scripts/Map/Room/EventRoom/BloodDonationRoom.cs
+++ Assets/01.Scripts/Map/Room/EventRoom/BloodDonationRoom.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class BloodDonationRoom : RoomBase
 {
+    private GameObject statue = null;
+    private GameObject minimapIcon = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        SpawnStatue();
+        minimapIcon = Managers.Resource.Instantiate("Assets/03.Prefabs/MinimapIcon/StatueRoomIcon.prefab", transform);
+        minimapIcon.transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y + 1.5f);
+        minimapIcon.SetActive(false);
+    }
+
     protected override void IsClear()
     {
+        isClear = true;
+    }
 
+    protected override void ShowIcon()
+    {
+        base.ShowIcon();
+        minimapIcon.SetActive(true);
+    }
+
+    protected override void SetRoomTypeFlag()
+    {
+        roomTypeFlag = Define.RoomTypeFlag.EventRoom;
+    }
+
+    public void SpawnStatue()
+    {
+        statue = Managers.Resource.Instantiate("Assets/03.Prefabs/Statue/BloodDonationStatue.prefab", transform);
     }
 }
