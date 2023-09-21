92858350deccaf01e217a711449434372a677854
김세현 <77712394+sehyeon1104@users.noreply.github.com>
2023-09-21 14:31:39 +0900

    플레이어 베이스 고스트로바꿈

--- Assets/01.Scripts/Player/PlayerBase.cs
+++ Assets/01.Scripts/Player/PlayerBase.cs
@@ -25,7 +25,7 @@ public class PlayerBase
         playerSkillNum = new int[] { 4, 5 };
         _fragmentAmount = 0;
         _fragmentAddAcq = 1f;
-        playerTransformTypeFlag = Define.PlayerTransformTypeFlag.Power;
+        playerTransformTypeFlag = Define.PlayerTransformTypeFlag.Ghost;
         AttackRange = 1.3f;
         InitAttackRange = AttackRange;
         PlayerTransformDataSOList = new List<PlayerSkillData>();
