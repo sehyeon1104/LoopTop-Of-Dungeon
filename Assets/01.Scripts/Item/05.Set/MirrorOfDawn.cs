7286f5a524a46c150c3a356900dbb9b64d5f1f2e
Ggm-gijoo <gijoo1012@gmail.com>
2023-09-25 00:05:14 +0900

    일식 이펙트, 센터씬 조정, 저주받은 반지 효과 변경

--- Assets/01.Scripts/Item/05.Set/MirrorOfDawn.cs
+++ Assets/01.Scripts/Item/05.Set/MirrorOfDawn.cs
@@ -22,11 +22,14 @@ public class MirrorOfDawn : ItemBase
 
     public override void Init()
     {
-
     }
 
     public override void Use()
     {
-
+        Debug.Log("여명의 거울 준비 완료");
+        GameManager.Instance.Player.playerBase.Attack += GameManager.Instance.Player.playerBase.InitAttack * 0.3f;
+        GameManager.Instance.Player.playerBase.AttackRange += GameManager.Instance.Player.playerBase.InitAttackRange * 0.3f;
+        GameManager.Instance.Player.playerBase.AttackSpeed += GameManager.Instance.Player.playerBase.InitAttackSpeed * 0.3f;
+        GameManager.Instance.Player.playerBase.SkillCoolDown += 20;
     }
 }
