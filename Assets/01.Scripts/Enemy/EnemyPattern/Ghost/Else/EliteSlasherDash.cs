641948c5aa22a06f72e0174496722e49b1847c62
Ggm-gijoo <gijoo1012@gmail.com>
2023-10-10 16:21:50 +0900

    엘몹끝

---
+++ Assets/01.Scripts/Enemy/EnemyPattern/Ghost/Else/EliteSlasherDash.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliteSlasherDash : MonoBehaviour
+{
+    BoxCollider2D getCol;
+    [SerializeField] GameObject warning;
+
+    private void Awake()
+    {
+        getCol = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnDisable()
+    {
+        Collider2D col = Physics2D.OverlapBox(transform.position, new Vector2(50,0.5f), warning.transform.eulerAngles.z, 1<<8);
+        if (col != null)
+            GameManager.Instance.Player.OnDamage(15, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector2(50, 0.5f));
+    }
+}
