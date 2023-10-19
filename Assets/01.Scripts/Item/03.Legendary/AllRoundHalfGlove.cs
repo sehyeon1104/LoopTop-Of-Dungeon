c947ba86f29d5090e0e7cef8ddc18e4e512925a5
Ggm-gijoo <gijoo1012@gmail.com>
2023-10-19 17:13:49 +0900

    아이템 수치 조정

--- Assets/01.Scripts/Item/03.Legendary/AllRoundHalfGlove.cs
+++ Assets/01.Scripts/Item/03.Legendary/AllRoundHalfGlove.cs
@@ -14,15 +14,17 @@ public class AllRoundHalfGlove : ItemBase
 
     public override void Use()
     {
-        Debug.Log("만능 하프글러브 효과 발동");
-        Debug.Log("공격력 15% 증가 및 스킬 쿨타임 15% 감소");
-        GameManager.Instance.Player.playerBase.Attack += GameManager.Instance.Player.playerBase.InitAttack * 0.15f;
-        GameManager.Instance.Player.playerBase.SkillCoolDown += 15;
+        GameManager.Instance.Player.playerBase.MaxHp += 10;
+        GameManager.Instance.Player.playerBase.Attack += GameManager.Instance.Player.playerBase.InitAttack * 0.3f;
+        GameManager.Instance.Player.playerBase.MoveSpeed += GameManager.Instance.Player.playerBase.InitMoveSpeed * 0.15f;
+        GameManager.Instance.Player.playerBase.CritChance += 5f;
     }
 
     public override void Disabling()
     {
-        GameManager.Instance.Player.playerBase.Attack -= GameManager.Instance.Player.playerBase.InitAttack * 0.15f;
-        GameManager.Instance.Player.playerBase.SkillCoolDown += 15;
+        GameManager.Instance.Player.playerBase.MaxHp -= 10;
+        GameManager.Instance.Player.playerBase.Attack -= GameManager.Instance.Player.playerBase.InitAttack * 0.3f;
+        GameManager.Instance.Player.playerBase.MoveSpeed -= GameManager.Instance.Player.playerBase.InitMoveSpeed * 0.15f;
+        GameManager.Instance.Player.playerBase.CritChance -= 5f;
     }
 }
